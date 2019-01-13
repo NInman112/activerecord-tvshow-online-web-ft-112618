f6a1a6c45944d04281fa2c33a5bb6cebdc34bb44
Nick Inman <nick@theoatmeal.com>
2019-01-13 02:41:28 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,6 +6,9 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     Show.where("rating = ?", self.highest_rating).first
+  end
 
+  def self.lowest_rating
+    Show.minimum(:rating)
   end
 end
